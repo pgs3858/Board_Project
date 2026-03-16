ba5fc82ac277deb3f61dd75c9dd54f79f85aadd4
X <geunseok@Xui-MacBookAir.local>
2026-03-16 10:20:21 +0900

    refactor: 서버 실행 테스트 코드 및 패키지, 클래스 파일 삭제

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,15 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static com.example.demo.testcontroller.TestController.testMethod;
-
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		testMethod();
 	}
 
 }
--- src/main/java/com/example/demo/testcontroller/TestController.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.testcontroller;
-
-public class TestController {
-
-    public static void testMethod() {
-        System.out.println("test입니다.");
-    }
-}
